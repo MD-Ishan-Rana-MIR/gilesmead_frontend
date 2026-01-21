8f11e50256bc83464829859a60a9a2feba52425b
ishanrana363 <ishanrana363@gmail.com>
2026-01-21 14:40:15 +0600

    header sticky

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Header = () => {
+    // sticky top 
     return (
         <div className="sticky top-0 z-50 my-4 px-4 bg-white pb-6 ">
             <h1 className="md:text-4xl font-semibold text-[#1F2937] text-lg">
