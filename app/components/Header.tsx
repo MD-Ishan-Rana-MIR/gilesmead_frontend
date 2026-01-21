65021b341f428b64e045d766dac6ec48ea4f22d2
ishanrana363 <ishanrana363@gmail.com>
2026-01-21 16:53:56 +0600

    logo size reduce

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="sticky top-0 z-50 my-4 px-4 bg-white pb-6 ">
             <div className=' flex items-center gap-x-4 md:gap-x-6 ' >
-                <div className=' md:w-12 md:h-12 w-10 h-10   rounded-full bg-[#4F7FD6] ' >
+                <div className=' md:w-8 md:h-8 w-6 h-6   rounded-full bg-[#4F7FD6] ' >
 
                 </div>
                 <div>
