7befdd72a368fba697c14de9fb004bd7c6fe1fe4
ishanrana363 <ishanrana363@gmail.com>
2026-01-21 17:16:15 +0600

    change text

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { TOPICS } from "./data/forcast";
 export default function SignalsPage() {
   return (
     <div>
-      <h1 className="text-xl md:text-2xl font-semibold mb-4 text-[#1F2937] ">Live Predictions of Outcomes</h1>
+      <h1 className="text-xl md:text-2xl font-semibold mb-4 text-[#1F2937] ">Live probabilities of outcomes.</h1>
       <MarketTable markets={markets} />
       <ForcastCard TOPICS={TOPICS} />
 
