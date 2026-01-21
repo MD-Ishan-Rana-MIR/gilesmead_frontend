033a62ee08a13628c0e8df9a5bbef858f1527ec8
ishanrana363 <ishanrana363@gmail.com>
2026-01-21 15:34:31 +0600

    logo change

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,9 +4,16 @@ const Header = () => {
     // sticky top 
     return (
         <div className="sticky top-0 z-50 my-4 px-4 bg-white pb-6 ">
-            <h1 className="md:text-4xl font-semibold text-[#4F7FD6]  text-lg">
-                Outcome.bet
-            </h1>
+            <div className=' flex items-center gap-x-4 md:gap-x-6 ' >
+                <div className=' md:w-14 w-10 h-10  md:h-14 w-  rounded-full bg-[#4F7FD6] ' >
+
+                </div>
+                <div>
+                    <h1 className="md:text-4xl font-semibold text-[#1F2937]  text-lg">
+                        Outcome.bet
+                    </h1>
+                </div>
+            </div>
         </div>
     )
 }
